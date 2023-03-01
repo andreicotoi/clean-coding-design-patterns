c6fce4033a87863813be478cf7508a04a6d8d0db
Andrei Cotoi <andreicotoi@Andreis-MacBook-Pro.local>
2023-03-01 10:20:43 +0200

    updates

---
+++ seminareCTS/seminar2/src/cts/s02/principii_clean_code/clase/readers/AplicantReader.java
@@ -0,0 +1,9 @@
+package cts.s02.principii_clean_code.clase.readers;
+import cts.s02.principii_clean_code.clase.Aplicant;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public interface AplicantReader {
+    List<Aplicant> readAplicants(String filename) throws FileNotFoundException;
+}
--- seminareCTS/seminar2/src/cts/s02/principii_clean_code/main/Program.java
+++ seminareCTS/seminar2/src/cts/s02/principii_clean_code/main/Program.java
@@ -4,16 +4,22 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import cts.s02.principii_clean_code.clase.Angajat;
+import cts.s02.principii_clean_code.clase.Aplicant;
 import cts.s02.principii_clean_code.clase.Utils;
+import cts.s02.principii_clean_code.clase.readers.AngajatReader;
+import cts.s02.principii_clean_code.clase.readers.AplicantReader;
+import cts.s02.principii_clean_code.clase.readers.ElevReader;
 
 public class Program {
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaElevi;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			AplicantReader aplicantReader = new ElevReader();
+
+			listaElevi = aplicantReader.readAplicants("elevi.txt");
+			for(Aplicant aplicant :listaAngajati)
+				System.out.println(aplicant.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
