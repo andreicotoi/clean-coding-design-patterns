dca617d1177553af219b3203324dfb6aab9d5468
Andrei Cotoi <cotoiandreigeorgel@gmail.com>
2023-03-30 07:22:42 +0300

    changed the main folder

---
+++ seminar2/src/cts/s02/principii_clean_code/main/Program.java
@@ -0,0 +1,30 @@
+package cts.s02.principii_clean_code.main;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import cts.s02.principii_clean_code.clase.Angajat;
+import cts.s02.principii_clean_code.clase.Aplicant;
+import cts.s02.principii_clean_code.clase.Utils;
+import cts.s02.principii_clean_code.clase.readers.AngajatReader;
+import cts.s02.principii_clean_code.clase.readers.AplicantReader;
+import cts.s02.principii_clean_code.clase.readers.ElevReader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaElevi;
+		try {
+			AplicantReader aplicantReader = new ElevReader();
+			listaElevi = aplicantReader.readAplicants("elevi.txt");
+			for(Aplicant aplicant : listaElevi) {
+				System.out.println(aplicant.toString());
+				aplicant.afisareFinantare();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
